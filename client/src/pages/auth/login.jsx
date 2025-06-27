d437c3adb9547f2c88dbaaa7a46016ab5cfc75f4
Awijeet-Singh15 <166923941+Awijeet-Singh15@users.noreply.github.com>
2025-06-27 11:19:10 +0530

    login

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -45,7 +45,7 @@ function AuthLogin() {
   }
 
   return (
-    <div className="mx-auto w-full max-w-md px-4 py-2 lg:py-8  sm:px-6 md:px-8 lg:px-10 z-50">
+    <div className="mx-auto max-h-screen overflow-hidden w-full max-w-md px-4 py-2 lg:py-8  sm:px-6 md:px-8 lg:px-10 z-50">
       <div className="text-center">
         <h1 className="text-3xl sm:text-4xl font-bold tracking-tight text-white">
           Login to your account
